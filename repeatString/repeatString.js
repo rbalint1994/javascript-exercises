94d174f47e883a22b627a98bc105c6494e49f412
rbalint1994 <77434447+rbalint1994@users.noreply.github.com>
2021-02-14 10:04:19 +0100

    Update repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const array = [];
+const repeatString = function(str, num){
+    for(let i = 0; i < num; i++){
+       array.push(str); 
+    }
+    console.log(array.join(''));
+}
 }
 
 module.exports = repeatString
