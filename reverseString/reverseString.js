f21343af5a2aca9fd8121f84111515d5733240f0
rbalint1994 <77434447+rbalint1994@users.noreply.github.com>
2021-02-15 14:28:21 +0100

    Update reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reversed = [];
+const reverseString = function(str) {
+  let splitted = str.split('')
+   for(let i = splitted.length - 1; i >= 0; i--){
+       reversed.push(splitted[i]);
+        }
+        return reversed.join("");
+}
 }
 
 module.exports = reverseString
